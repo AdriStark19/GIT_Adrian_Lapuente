1e0d80f5667a4b554154cd266da04b93735589ea
Adrian Lapuente <adrian-19-@hotmail.com>
2022-11-09 08:55:08 +0100

    "Modificación 1"

--- GIT_Adrian_Lapuente/src/AL/AL.java
+++ GIT_Adrian_Lapuente/src/AL/AL.java
@@ -5,6 +5,7 @@ public class AL {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Alumno: Adrian Lapuente");
+		System.out.println("Primera Modificación desde Master");
 	}
 
 }
